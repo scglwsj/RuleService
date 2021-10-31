d8ed1340b09ab269f16705a1fc3c77d944e60da4
shiji <scglwsj@163.com>
2021-10-31 23:26:44 +0800

    数组参数的规则校验

---
+++ RuleService/Rules/PartnerUtil.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace RuleService.Rules
+{
+    public static class PartnerUtil
+    {
+        private static readonly List<string> ValidPartnerNames = new()
+        {
+            "AS", "DF", "GH"
+        };
+
+        public static bool ValidatePartner(string partnerName) => ValidPartnerNames.Contains(partnerName);
+    }
+}
--- RuleService/Rules/RuleService.cs
+++ RuleService/Rules/RuleService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using RulesEngine.Extensions;
 using RulesEngine.Models;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace RuleService.Rules
 {
@@ -16,76 +17,122 @@ namespace RuleService.Rules
         public RuleService()
         {
             const string data = @"
-                [
-                    {
-                        ""WorkflowName"": ""Register"",
-                        ""Rules"": [
-                            {
-                                ""RuleName"": ""Give100Points"",
-                                ""SuccessEvent"": ""Register success and earn 100 points"",
-                                ""RuleExpressionType"": ""LambdaExpression"",
-                                ""Expression"": ""true""
-                            }
-                        ]
-                    },
-                    {
-                        ""WorkflowName"": ""Payment"",
-                        ""Rules"": [
-                            {
-                                ""RuleName"": ""GivePointsByPayment"",
-                                ""SuccessEvent"": ""Pay success and earn {0} points"",
-                                ""RuleExpressionType"": ""LambdaExpression"",
-                                ""Expression"": ""true"",
-                                ""Actions"": {
-                                    ""OnSuccess"": {
-                                        ""Name"": ""OutputExpression"",
-                                        ""Context"": {
-                                            ""Expression"": ""subtotal""
-                                        }
-                                    }
-                                }
-                            },
-                            {
-                                ""RuleName"": ""MultiplyPoints"",
-                                ""Operator"": ""Or"",
-                                ""Rules"": [
-                                    {
-                                        ""RuleName"": ""MoreThan200"",
-                                        ""ErrorMessage"": ""subtotal is less than or equal to 200"",
-                                        ""Expression"": ""subtotal > 200""
-                                    },
-                                    {
-                                        ""RuleName"": ""DoubleEleven"",
-                                        ""RuleExpressionType"": ""LambdaExpression"",
-                                        ""Expression"": ""month == 11 AND date == 11""
-                                    }
-                                ],
-                                ""Actions"": {
-                                    ""OnSuccess"": {
-                                        ""Name"": ""OutputExpression"",
-                                        ""Context"": {
-                                            ""Expression"": ""subtotal""
-                                        }
+            [
+                {
+                    ""WorkflowName"": ""Register"",
+                    ""Rules"": [
+                        {
+                            ""RuleName"": ""Give100Points"",
+                            ""SuccessEvent"": ""Register success and earn 100 points"",
+                            ""RuleExpressionType"": ""LambdaExpression"",
+                            ""Expression"": ""true""
+                        }
+                    ]
+                },
+                {
+                    ""WorkflowName"": ""Transformation"",
+                    ""Rules"": [
+                        {
+                            ""RuleName"": ""TransForMore"",
+                            ""SuccessEvent"": ""Transfer points succeed"",
+                            ""RuleExpressionType"": ""LambdaExpression"",
+                            ""Expression"": ""PartnerUtil.ValidatePartner(partnerName)"",
+                            ""ErrorMessage"": ""Transfer points failed, invalid partner name"",
+                            ""ErrorType"": ""Error""
+                        }
+                    ]
+                },
+                {
+                    ""WorkflowName"": ""Payment"",
+                    ""Rules"": [
+                        {
+                            ""RuleName"": ""GivePointsByPayment"",
+                            ""SuccessEvent"": ""Pay success and earn {0} points"",
+                            ""RuleExpressionType"": ""LambdaExpression"",
+                            ""Expression"": ""true"",
+                            ""Actions"": {
+                                ""OnSuccess"": {
+                                    ""Name"": ""OutputExpression"",
+                                    ""Context"": {
+                                        ""Expression"": ""subtotal""
                                     }
                                 }
                             }
-                        ]
-                    }
-                ]";
-
+                        },
+                        {
+                            ""RuleName"": ""MultiplyPoints"",
+                            ""Operator"": ""Or"",
+                            ""Rules"": [
+                                {
+                                    ""RuleName"": ""MoreThan200"",
+                                    ""ErrorMessage"": ""subtotal is less than or equal to 200"",
+                                    ""Expression"": ""subtotal > 200""
+                                },
+                                {
+                                    ""RuleName"": ""DoubleEleven"",
+                                    ""RuleExpressionType"": ""LambdaExpression"",
+                                    ""Expression"": ""month == 11 AND date == 11""
+                                }
+                            ],
+                            ""Actions"": {
+                                ""OnSuccess"": {
+                                    ""Name"": ""OutputExpression"",
+                                    ""Context"": {
+                                        ""Expression"": ""subtotal""
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                },
+                {
+                    ""WorkflowName"": ""Events"",
+                    ""Rules"": [
+                        {
+                            ""RuleName"": ""Completed one of the events"",
+                            ""ErrorMessage"": ""None of the events is completed"",
+                            ""ErrorType"": ""Error"",
+                            ""SuccessEvent"": ""You got the event points!"",
+                            ""RuleExpressionType"": ""LambdaExpression"",
+                            ""localParams"": [
+                                {
+                                    ""name"": ""completedEvents"",
+                                    ""expression"": ""events.Where(Status == \""Completed\"")""
+                                }
+                            ],
+                            ""Expression"": ""completedEvents.Any()""
+                        }
+                    ]
+                }
+            ]";
             var workflowRules = JsonConvert.DeserializeObject<WorkflowRules[]>(data);
-            _engine = new RulesEngine.RulesEngine(workflowRules);
+
+            var reSettings = new ReSettings
+            {
+                CustomTypes = new[] {typeof(PartnerUtil)}
+            };
+            _engine = new RulesEngine.RulesEngine(workflowRules, null, reSettings);
         }
 
         public async Task<RuleResponse> Verify(string workFlowName, JsonElement args)
         {
             var input = new List<RuleParameter>();
+
             switch (workFlowName)
             {
                 case "Payment":
                     input.Add(new RuleParameter("subtotal", args.GetProperty("subtotal").GetDecimal()));
                     input.Add(new RuleParameter("month", args.GetProperty("month").GetInt32()));
                     input.Add(new RuleParameter("date", args.GetProperty("date").GetInt32()));
+
+                    break;
+                case "Transformation":
+                    input.Add(new RuleParameter("partnerName", args.GetProperty("partnerName").GetString()));
+                    break;
+                case "Events":
+                    input.Add(new RuleParameter("events", JsonSerializer.Deserialize<List<Event>>(
+                        args.GetProperty("events").GetRawText(),
+                        new JsonSerializerOptions {PropertyNameCaseInsensitive = true})));
                     break;
             }
 
@@ -98,10 +145,18 @@ namespace RuleService.Rules
                     .FindAll(ruleResult => ruleResult.ActionResult != null)
                     .Select(ruleResult => ruleResult.ActionResult.Output)
                     .Sum(Convert.ToDecimal);
-                response = new RuleResponse() {Message = string.Format(message, actionResult)};
+                response = new RuleResponse {Message = string.Format(message, actionResult)};
             });
+
+            resultList.OnFail(() => { response = new RuleResponse {Message = "Trigger rule failed"}; });
 
             return response;
         }
+    }
+
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
     }
 }
